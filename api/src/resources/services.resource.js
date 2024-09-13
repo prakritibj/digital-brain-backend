13b1ff0ac9b5f509cc26520a7596c31d7937a90a
prakritibj <prakritibhardwaj9630@gmail.com>
2024-09-13 16:28:00 +0530

    apis done

--- api/src/resources/services.resource.js
+++ api/src/resources/services.resource.js
@@ -8,5 +8,25 @@ resourseService.createResourse = async ({name,subcategoryID, link})=>{
     //  console.log(newResorse, "newResorse")
 
 }
+// subcategoryID ya isi ki ID
+resourseService.getAllResourse = async (id) => {
+    return await resourse.find({id});
+};
+resourseService.resourseExists = async (name) => {
+    return await resourse.findOne({name});
+};
+// Delete a category by ID
+resourseService.deleteResousrse = async (id) => {
+    return await resourse.findByIdAndDelete(id)
+};
+
+// update a category by ID
+resourseService.updateCategory = async (id, updates) => {
+return await resourse.findByIdAndUpdate(id, updates, { new: true });
+}
 
 module.exports = resourseService
+
+// ===============================================================
+
+
--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser")
 app.use(bodyParser.json())
 const cors = require('cors');
 const corsOptions = {
-  origin: 'http://localhost:5173', 
+  origin: '*', 
   methods: 'GET,PUT,PATCH,POST,DELETE', 
   allowedHeaders: 'Content-Type, Authorization', 
   credentials: true, 
